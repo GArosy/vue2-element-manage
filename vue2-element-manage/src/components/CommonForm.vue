e43535616ec5bcc585c1dba6ae01662fcf09cb10
Gsy <837225481@qq.com>
2022-06-29 21:36:40 +0800

    CommonForm 组件编写

---
+++ vue2-element-manage/src/components/CommonForm.vue
@@ -0,0 +1,57 @@
+<template>
+  <el-form ref="form" label-width="100px" :model="form" :inline="inline">
+    <el-form-item
+      v-for="item in formLabel"
+      :key="item.label"
+      :label="item.label"
+    >
+      <el-input
+        v-if="item.type === 'input'"
+        :placeholder="`请输入${item.label}`"
+        v-model="form[item.model]"
+      >
+      </el-input>
+      <el-switch v-if="item.type === 'switch'" v-model="form[item.model]">
+      </el-switch>
+      <el-date-picker
+        v-if="item.type === 'date'"
+        type="date"
+        value-format="yyyy-MM-dd"
+        placeholder="选择日期"
+        v-model="form[item.model]"
+      ></el-date-picker>
+      <el-select
+        v-if="item.type === 'select'"
+        placeholder="请选择"
+        v-model="form[item.model]"
+      >
+        <el-option
+          v-for="optItem in item.opts"
+          :key="optItem.value"
+          :label="optItem.label"
+          :value="optItem.value"
+        ></el-option>
+      </el-select>
+    </el-form-item>
+    <!-- 如果以上组件都不满足，使用自定义插槽 -->
+    <el-form-item>
+      <slot></slot>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  name: "CommonForm",
+  props: {
+    formLabel: Array,
+    form: Object,
+    inline: Boolean,
+  },
+  data() {
+    return {
+
+    }
+  }
+};
+</script>
