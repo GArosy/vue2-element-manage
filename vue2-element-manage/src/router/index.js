8d47d78ee273fa64f91116d1e83dc04cab28b4b9
GArosy <837225481@qq.com>
2022-07-13 19:57:38 +0800

    更改导入页面的方式

--- vue2-element-manage/src/router/index.js
+++ vue2-element-manage/src/router/index.js
@@ -1,73 +1,63 @@
 // 全局引入router
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-// 导入要路由的组件
-import Login from '../pages/Login.vue';
-import Main from '../pages/Main.vue';
-import Home from '../pages/Home.vue';
-import User from '../pages/User.vue';
-import Mall from '../pages/Mall.vue';
-import Other1 from '../pages/OtherPage1.vue';
-import Other2 from '../pages/OtherPage2.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
-
 // 定义一些路由，将以上组件映射到路由
 const routes = [
-    // 定义根目录下的路由，component为按需引入的组件页面home
-    {
-        path: '/',
-        name: 'main',
-        component: Main,
-        // 路由嵌套
-        children: [
-            {
-                path: '/home',
-                name: 'home',
-                label: '首页', 
-                component: Home
-            },
-            {
-                path: '/user',
-                name: 'user',
-                label: '用户管理',
-                component: User
-            }, 
-            {
-                path: '/mall',
-                name: 'mall',
-                label: '商品管理',
-                component: Mall
-            },
-            {
-                path: '/page1',
-                name: 'page1',
-                label: '其它1',
-                component: Other1
-            },
-            {
-                path: '/page2',
-                name: 'page2',
-                label: '其它2',
-                component: Other2
-            },
-        ]
-    },
-    {
-        path: '/login',
-        name: 'login',
-        label: '登录',
-        component: Login
-    }
-
-]
+  // 定义根目录下的路由，component为按需引入的组件页面home
+  {
+    path: "/",
+    name: "main",
+    component: () => import("@/pages/Main"),
+    // 路由嵌套
+    children: [
+      {
+        path: "/home",
+        name: "home",
+        label: "首页",
+        component: () => import("@/pages/Home"),
+      },
+      {
+        path: "/user",
+        name: "user",
+        label: "用户管理",
+        component: () => import("@/pages/User"),
+      },
+      {
+        path: "/mall",
+        name: "mall",
+        label: "商品管理",
+        component: () => import("@/pages/Mall"),
+      },
+      {
+        path: "/page1",
+        name: "page1",
+        label: "其它1",
+        component: () => import("@/pages/OtherPage1"),
+      },
+      {
+        path: "/page2",
+        name: "page2",
+        label: "其它2",
+        component: () => import("@/pages/OtherPage2"),
+      },
+    ],
+  },
+  {
+    path: "/login",
+    name: "login",
+    label: "登录",
+    component: () => import("@/pages/Login"),
+  },
+];
 
 // 创建路由实例并传递 `routes` 配置
 const router = new VueRouter({
-    mode: 'history',
-    routes // `routes: routes` 的缩写
-})
+  mode: "history",
+  routes, // `routes: routes` 的缩写
+});
 
 // 导出
-export default router
+export default router;
