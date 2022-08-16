154f55c9b831bdabb234fb80093088f6064859e5
GArosy <837225481@qq.com>
2022-08-16 16:57:40 +0800

    配置跨域（代理方法）

--- vue2-element-manage/vue.config.js
+++ vue2-element-manage/vue.config.js
@@ -5,6 +5,15 @@ module.exports = defineConfig({
   publicPath: '/',  // 公共路径
   devServer: {  // 配置服务器
     port: 8080, // 端口号
-    open: true, // 自动打开浏览器
+    proxy: {  // 前后端分离，需要配置跨域（代理方法）
+      '/api': { // 如果请求地址以api开头，就触发代理机制
+        target: 'http://localhost:3000',  // 要代理的接口地址
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {  // 重写路径
+          '^/api': ''
+        }
+      }
+    }
   }
 })
