d2207d27e5cd6acb7f3bd8662a298e675755ec7a
GArosy <837225481@qq.com>
2022-08-16 16:57:58 +0800

    添加商品页路径配置

--- vue2-element-manage/src/api/base.js
+++ vue2-element-manage/src/api/base.js
@@ -4,7 +4,13 @@
 
 const base = {
   // 商品列表
-  goodsList:'/api/mall/getGoods', 
+  goodsList: '/api/mall/getGoods', 
+  // 添加商品
+  createGoods: '/api/mall/createGoods',
+  // 编辑商品
+  editGoods: '/api/mall/editGoods',
+  // 删除商品
+  delGoods: '/api/mall/delGoods'
 }
 
 export default base;
