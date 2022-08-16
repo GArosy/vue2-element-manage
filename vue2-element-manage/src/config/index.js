8a83ffa59f7eb9ee21d226a1fdb303cb08b0d386
GArosy <837225481@qq.com>
2022-08-16 19:22:02 +0800

    配置生产环境baseURL

--- vue2-element-manage/src/config/index.js
+++ vue2-element-manage/src/config/index.js
@@ -1,8 +1,17 @@
 export default {
     baseURL: {
+        /**
+         * - baseURL会拼接在每个axios请求之前，可触发devServer代理实现开发环境的跨域
+         * - baseURL不能为绝对路径，会导致devServer.proxy失效
+         * - 如果项目部署路径不在根目录下
+         *      baseURL必须带 / 前缀
+         *      例如项目部署在 https://example.com/licenses/ ，发送 axios.get('/licenses') 请求
+         *      baseURL = '/api' => https://example.com/api/licenses
+         *      baseURL = 'api' => https://example.com/licenses/api/licenses
+         */
         // 开发环境
-        dev: '/api/',
+        dev: '/api',
         // 生产环境
-        prod: ''
+        prod: '/api'
     }
 }
