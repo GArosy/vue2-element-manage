45ee9ad54868779afc4bfb9136f9da4ae45ce663
GArosy <837225481@qq.com>
2022-08-24 17:52:52 +0800

    移除

--- vue2-element-manage/src/utils/uuid.js
+++
@@ -1,8 +0,0 @@
-const uuidGenerater = () => {
-  let temp_url = URL.createObjectURL(new Blob());
-  let uuid = temp_url.toString();
-  URL.revokeObjectURL(temp_url);
-  return uuid.substring(uuid.lastIndexOf('/') + 1)
-}
-
-export default uuidGenerater
