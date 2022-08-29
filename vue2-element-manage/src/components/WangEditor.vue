292f7bf45035ddf0e4514534cb6a10848dce41f3
GArosy <837225481@qq.com>
2022-08-29 17:41:49 +0800

    完成商品描述功能

--- vue2-element-manage/src/components/WangEditor.vue
+++ vue2-element-manage/src/components/WangEditor.vue
@@ -7,11 +7,12 @@
       :mode="mode"
     />
     <Editor
-      style="height: 500px; overflow-y: hidden"
+      style="height: 200px; overflow-y: hidden"
       v-model="html"
       :defaultConfig="editorConfig"
       :mode="mode"
       @onCreated="onCreated"
+      @onChange="onChange"
     />
   </div>
 </template>
@@ -19,12 +20,33 @@
 import { Editor, Toolbar } from "@wangeditor/editor-for-vue";
 
 export default {
+  props: { value: String },
   components: { Editor, Toolbar },
   data() {
     return {
       editor: null,
-      html: "<p>hello</p>",
-      toolbarConfig: {},
+      html: this.value,
+      text: "",
+      toolbarConfig: {
+        // 菜单栏配置
+        toolbarKeys: [
+          "bold",
+          "italic",
+          "underline",
+          "color",
+          "headerSelect",
+          "|",
+          "justifyLeft",
+          "justifyCenter",
+          "justifyRight",
+          "divider",
+          "|",
+          "clearStyle",
+          "undo",
+          "redo",
+          "fullScreen",
+        ],
+      },
       editorConfig: { placeholder: "请输入内容..." },
       mode: "default", // or 'simple'
     };
@@ -32,13 +54,26 @@ export default {
   methods: {
     onCreated(editor) {
       this.editor = Object.seal(editor); // 一定要用 Object.seal() ，否则会报错
+      // console.log("editor", editor);
+      // console.log("menukeys", editor.getAllMenuKeys()); // 查看所有keys
+      // console.log("getConfig", editor.getConfig().MENU_CONF); // 查看当前菜单配置
+    },
+    onChange(editor) {
+      this.text = editor.getText();
+      this.$emit("send", { html: this.html, text: this.text });
+    },
+  },
+  // 监听父组件传来的value，发生改变则更新编辑器内容
+  watch: {
+    value(newVal, oldVal) {
+      this.html = newVal;
     },
   },
   mounted() {
     // 模拟 ajax 请求，异步渲染编辑器
-    setTimeout(() => {
-      this.html = "<p>模拟 Ajax 异步设置内容 HTML</p>";
-    }, 1500);
+    // setTimeout(() => {2
+    //   this.html = "<p>模拟 Ajax 异步设置内容 HTML</p>";
+    // }, 1500);
   },
   beforeDestroy() {
     const editor = this.editor;
